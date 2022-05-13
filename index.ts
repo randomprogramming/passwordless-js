885573ea89b2bde0c997dcae5f60bc3ff1210188
randomprogramming <dgoluban38@gmail.com>
2022-05-13 11:19:29 +0200

    maybe fix packaging

--- index.ts
+++
@@ -1,4 +0,0 @@
-import register from "./src/register";
-import { CredentialError, ServerError } from "./src/exceptions";
-
-export default { register, CredentialError, ServerError };
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import register from "./register";
+import { CredentialError, ServerError } from "./exceptions";
+
+export default { register, CredentialError, ServerError };
