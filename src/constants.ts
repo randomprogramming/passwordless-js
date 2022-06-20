a49a0ed5da3ef6f27e3581cbb815e2a1ad110331
randomprogramming <dgoluban38@gmail.com>
2022-06-20 22:03:51 +0200

    new api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
-export const PASSWORDLESS_SERVER_BASE_URL =
-  "http://unpassapp-env.eba-rqisn7ep.eu-west-1.elasticbeanstalk.com/api";
+export const PASSWORDLESS_SERVER_BASE_URL = "https://aws.unpass.me/api";
